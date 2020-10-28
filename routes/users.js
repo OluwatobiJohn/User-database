08c92b9f3de08138b7b57a07b18d6b40324dad7e
DELL <you@example.com>
2020-10-28 13:23:10 +0100

    log in post req

---
+++ routes/users.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+
+//Get Log in and Sign Up
+router.get('/login', (req,res) => {
+    res.send('This is the login page');
+});
+
+router.get('/register', (req, res) => {
+    res.send('This is the Sign up page');
+});
+
+
+//Post Log in and Sign up
+router.post('/register', (req, res) => {
+  
+    const {name, email, password, password2} = req.body;
+    const errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({msg: 'All fields must be entered'});
+    };
+
+    if (password !== password2) {
+        errors.push({msg: 'Passwords do not match'});
+    };
+
+    if (password.length < 6) {
+        errors.push({msg: 'Password must be atleast 6 characters'})
+    };
+
+    if (errors.length > 0) {
+        return res.status(400).json({ errors: errors });
+    }  else {
+        //validation
+        User.findOne({email: email}).exec((err, user) => {
+
+            if (user) {
+                errors.push({msg: 'Email has already been registered'});
+                res.json({errors: errors});
+            } else {
+                const newUser = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                const saltRounds = 10;
+                const passwordText = newUser.password;
+                bcrypt.genSalt(saltRounds, (err, salt) => {
+                    bcrypt.hash(passwordText, salt, (err, hash) => {
+                        if (err) throw err
+                        //save password to hash
+                        newUser.password = hash;
+                        //save user
+                        newUser.save()
+                        .then((value) => {
+                            console.log(value);
+                            res.redirect('/users/login');
+                        })
+                    });
+                });
+
+            };
+        });
+    };
+
+
+});
+
+
+router.post('/login', (req, res) => {
+
+});
+
+
+//Get Log out
+router.get('/logout', (req, res) => {
+
+});
+
+module.exports = router;
